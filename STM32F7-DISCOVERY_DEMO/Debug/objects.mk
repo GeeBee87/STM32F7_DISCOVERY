4a16c56a64d064f3cf15bdfb13b4b4ef389ab8a3
Gabriele <Gabriele@GBOROCCI-LENOVO>
2017-06-16 10:57:15 +0200

    First Commit

---
+++ STM32F7-DISCOVERY_DEMO/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -l:STM32746G_Discovery_STemWin_Addons_GCC.a -l:STemWin532_CM7_OS_GCC_ot.a
+
